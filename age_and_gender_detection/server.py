f5b0618c91365361c4b831ef0b77ca3daff1a1ab
Prasanna <prasanna.lakkur@gmail.com>
2025-03-31 01:59:41 -0400

    Changed dir name

---
+++ age_and_gender_detection/server.py
@@ -0,0 +1,69 @@
+from flask_ml.flask_ml_server import MLServer
+from flask_ml.flask_ml_server.models import (
+    DirectoryInput,
+    InputSchema,
+    InputType,
+    TaskSchema,
+    ResponseBody,
+    TextResponse,
+)
+from typing import TypedDict
+from .model import AgeGenderDetector
+from logging import getLogger
+import json
+import argparse
+
+
+logger = getLogger(__name__)
+
+# Configure UI Elements in RescueBox Desktop
+def task_schema() -> TaskSchema:
+    input_schema = InputSchema(
+        key="image_directory",
+        label="Path to the directory containing all the images",
+        input_type=InputType.DIRECTORY,
+    )
+    return TaskSchema(inputs=[input_schema], parameters=[])
+
+
+# Specify the input and output types for the task
+class Inputs(TypedDict):
+    image_directory: DirectoryInput
+
+
+class Parameters(TypedDict):
+    pass
+
+
+server = MLServer(__name__)
+server.add_app_metadata(
+    name="Age and Gender Classifier",
+    author="UMass Rescue",
+    version="0.1.0",
+    info="Model to classify the age and gender of all faces in an image.",
+)
+model = AgeGenderDetector()
+
+
+@server.route(
+    "/predict",
+    task_schema_func=task_schema,
+    short_title="Age and Gender Classifier",
+    order=0
+)
+def predict(inputs: Inputs, parameters: Parameters) -> ResponseBody:
+    input_path = inputs["image_directory"].path
+    logger.info(f"Input path: {input_path}")
+    res_list = model.predict_age_and_gender_on_dir(input_path)
+    logger.info(f"Response: {res_list}")
+    response = TextResponse(value=json.dumps(res_list))
+    return ResponseBody(root=response)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run a server.")
+    parser.add_argument(
+        "--port", type=int, help="Port number to run the server", default=5000
+    )
+    args = parser.parse_args()
+    server.run(port=args.port)
